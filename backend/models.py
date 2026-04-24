627ed6132f9bf1ac8be79527198ae86084b309f0
Deva Harsha <devaharsha@Devas-MacBook-Air.local>
2026-04-24 20:30:23 +0530

    feat: pydantic models

---
+++ backend/models.py
@@ -0,0 +1,34 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class UserModel(BaseModel):
+    email: str
+    hashed_password: str
+
+class ClassModel(BaseModel):
+    name: str
+    description: str
+    schedule: str
+    image_url: str
+
+class ClassUpdateModel(BaseModel):
+    name: Optional[str] = None
+    description: Optional[str] = None
+    schedule: Optional[str] = None
+    image_url: Optional[str] = None
+
+class PricingModel(BaseModel):
+    name: str
+    best_price: bool = False
+    monthly: str
+    quarterly: str
+    half_yearly: str
+    yearly: str
+
+class PricingUpdateModel(BaseModel):
+    name: Optional[str] = None
+    best_price: Optional[bool] = None
+    monthly: Optional[str] = None
+    quarterly: Optional[str] = None
+    half_yearly: Optional[str] = None
+    yearly: Optional[str] = None
